c97a4aab2cd83875396a7ea44392b16bfb7b8567
Stephens6623 <stephenspargo134@gmail.com>
2025-06-19 20:29:21 -0400

    Update index.js
    served files to server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
+
+app.get('/answers', (req, res) => { 
+    res.sendFile(path.join(__dirname,'api','answers.json'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
